f8a1c8d2a1a9bd715fe8d08e956099590ae2b4a3
Stormbreaker20 <luvsharma.del@gmail.com>
2023-12-16 22:18:09 +0530

    AlgoViz

--- backend/main.py
+++ backend/main.py
@@ -7,14 +7,16 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173",
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 @app.get("/")
 async def root():
